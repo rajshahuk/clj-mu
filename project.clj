3f157cb5cc5fe8131d57e2c613fbeff14cba7019
Rajesh Shah <raj@12nines.com>
2020-12-08 16:45:37 +0000

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.twelvenines/clj-mu "0.0.15"
+(defproject com.twelvenines/clj-mu "0.0.16"
   :description "Clojure friendly library for using https://muserver.io"
   :url "https://github.com/rajshahuk/clj-mu"
   :license {:name "MIT License"
