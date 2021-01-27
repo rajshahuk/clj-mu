4abb7cc5d2f3bd44aaac2a95aa527934fac31f8d
Rajesh Shah <raj@12nines.com>
2021-01-27 22:24:43 +0000

    bumping version number

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.twelvenines/clj-mu "0.0.17"
+(defproject com.twelvenines/clj-mu "0.0.18"
   :description "Clojure friendly library for using https://muserver.io"
   :url "https://github.com/rajshahuk/clj-mu"
   :license {:name "MIT License"
             :url "https://github.com/rajshahuk/clj-mu/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.muserver/mu-server "0.56.3"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
+                 [io.muserver/mu-server "0.56.5"]
                  [org.clojure/tools.logging "1.1.0"]
                  [ch.qos.logback/logback-classic "1.2.3"]]
   :profiles {:test {:resource-paths ["test/resources"]
