c7f463dbf56273ffa5bc47696af05d3c6f1247e7
Rajesh Shah <raj@12nines.com>
2022-04-02 19:14:41 +0100

    updated version of clojure and mu

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.twelvenines/clj-mu "0.0.23"
+(defproject com.twelvenines/clj-mu "0.0.24"
   :description "Clojure friendly library for using https://muserver.io"
   :url "https://github.com/rajshahuk/clj-mu"
   :license {:name "MIT License"
             :url "https://github.com/rajshahuk/clj-mu/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [io.muserver/mu-server "0.72.7"]
+  :dependencies [[org.clojure/clojure "1.11.0"]
+                 [io.muserver/mu-server "0.72.9"]
                  [org.clojure/tools.logging "1.2.4"]
                  [ch.qos.logback/logback-classic "1.2.11"]]
   :profiles {:test {:resource-paths ["test/resources"]
