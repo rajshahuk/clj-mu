27bbca146fcabb5868454eba949a7cb5293aeb93
Rajesh Shah <raj@12nines.com>
2022-04-11 14:34:23 +0100

    upgrade minor version of clojure

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.twelvenines/clj-mu "0.0.24"
+(defproject com.twelvenines/clj-mu "0.0.25"
   :description "Clojure friendly library for using https://muserver.io"
   :url "https://github.com/rajshahuk/clj-mu"
   :license {:name "MIT License"
