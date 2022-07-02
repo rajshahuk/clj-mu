a066ca491497b492ae330bf95a77094998b51292
Rajesh Shah <raj@12nines.com>
2022-07-02 17:49:55 +0100

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject com.twelvenines/clj-mu "0.0.27"
+(defproject com.twelvenines/clj-mu "0.0.28"
   :description "Clojure friendly library for using https://muserver.io"
   :url "https://github.com/rajshahuk/clj-mu"
   :license {:name "MIT License"
             :url "https://github.com/rajshahuk/clj-mu/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [io.muserver/mu-server "0.72.9"]
+                 [io.muserver/mu-server "0.72.16"]
                  [org.clojure/tools.logging "1.2.4"]
                  [ch.qos.logback/logback-classic "1.2.11"]]
   :profiles {:test {:resource-paths ["test/resources"]
-                    :dependencies   [[cheshire "5.10.2"]
+                    :dependencies   [[cheshire "5.11.0"]
                                      [clj-http "3.12.3"]]}}
   :repl-options {:init-ns clj-mu.core})
