52d895d5af65fb1837b75581212a2e70a335a9c0
Rajesh Shah <raj@12nines.com>
2022-12-09 10:34:17 +0000

    fixing logging as a result of logback upgrade

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.twelvenines/clj-mu "0.0.29"
+(defproject com.twelvenines/clj-mu "0.0.30"
   :description "Clojure friendly library for using https://muserver.io"
   :url "https://github.com/rajshahuk/clj-mu"
   :license {:name "MIT License"
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [io.muserver/mu-server "0.72.19"]
                  [org.clojure/tools.logging "1.2.4"]
-                 [ch.qos.logback/logback-classic "1.4.4"]]
+                 [ch.qos.logback/logback-classic "1.4.5"]
+                 [org.slf4j/slf4j-api "2.0.5"]]
   :profiles {:test {:resource-paths ["test/resources"]
                     :dependencies   [[cheshire "5.11.0"]
                                      [clj-http "3.12.3"]]}}
