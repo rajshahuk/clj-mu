a26d6b87d18d40ab740bb3c8f16890044b51d5dc
Rajesh Shah <raj@12nines.com>
2023-09-30 11:56:28 +0100

    updated dependencies, renamed profile and moved logging to test depends

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "MIT License"
             :url "https://github.com/rajshahuk/clj-mu/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [io.muserver/mu-server "0.74.0"]
-                 [org.clojure/tools.logging "1.2.4"]
-                 [ch.qos.logback/logback-classic "1.4.8"]
-                 [org.slf4j/slf4j-api "2.0.7"]]
-  :profiles {:test {:resource-paths ["test/resources"]
-                    :dependencies   [[cheshire "5.11.0"]
-                                     [clj-http "3.12.3"]]}}
+                 [io.muserver/mu-server "0.74.2"]
+                 [org.clojure/tools.logging "1.2.4"]]
+  :profiles {:dev {:resource-paths ["test/resources"]
+                    :dependencies   [[cheshire "5.12.0"]
+                                     [clj-http "3.12.3"]
+                                     [org.slf4j/slf4j-api "2.0.9"]
+                                     [ch.qos.logback/logback-classic "1.4.11"]]}}
   :repl-options {:init-ns clj-mu.core})
