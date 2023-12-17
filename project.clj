55f23510ebf60010f121f5133c0be415480a9ed5
Rajesh Shah <raj@12nines.com>
2023-12-17 19:26:42 +0000

    updates to dependencies and moved to 0.1.0 as mu moves to 1.0.0

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject com.twelvenines/clj-mu "0.0.33"
+(defproject com.twelvenines/clj-mu "0.1.0"
   :description "Clojure friendly library for using https://muserver.io"
   :url "https://github.com/rajshahuk/clj-mu"
   :license {:name "MIT License"
             :url "https://github.com/rajshahuk/clj-mu/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [io.muserver/mu-server "0.74.2"]
+                 [io.muserver/mu-server "1.0.0"]
                  [org.clojure/tools.logging "1.2.4"]]
   :profiles {:dev {:resource-paths ["test/resources"]
                     :dependencies   [[cheshire "5.12.0"]
                                      [clj-http "3.12.3"]
                                      [org.slf4j/slf4j-api "2.0.9"]
-                                     [ch.qos.logback/logback-classic "1.4.11"]]}}
+                                     [ch.qos.logback/logback-classic "1.4.14"]]}}
   :repl-options {:init-ns clj-mu.core})
