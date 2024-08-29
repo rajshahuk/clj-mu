d694e47cd586bd8cc9c7790bb127fdcdb1b8b403
Rajesh Shah <raj@12nines.com>
2024-08-29 10:46:38 +0100

    fixing typo in version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.twelvenines/clj-mu "0.1.4gi ts"
+(defproject com.twelvenines/clj-mu "0.1.4"
   :description "Clojure friendly library for using https://muserver.io"
   :url "https://github.com/rajshahuk/clj-mu"
   :license {:name "MIT License"
