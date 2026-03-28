6d357404cd7c2bbbb709ab1725f5a7d40da63320
Rajesh Shah <raj@12nines.com>
2026-03-28 17:28:44 +0000

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/rajshahuk/clj-mu"
   :license {:name "MIT License"
             :url "https://github.com/rajshahuk/clj-mu/blob/master/LICENSE"}
-  :dependencies [[org.clojure/tools.logging "1.3.0"]]
+  :dependencies [[org.clojure/tools.logging "1.3.1"]]
   :profiles {:dev {:resource-paths ["test/resources"]
-                    :dependencies   [[org.clojure/clojure "1.12.1"]
-                                     [io.muserver/mu-server "2.1.10"]
-                                     [cheshire "6.0.0"]
-                                     [clj-http "3.13.0"]
+                    :dependencies   [[org.clojure/clojure "1.12.4"]
+                                     [io.muserver/mu-server "2.2.6"]
+                                     [cheshire "6.2.0"]
+                                     [clj-http "3.13.1"]
                                      [org.slf4j/slf4j-api "2.0.17"]
-                                     [ch.qos.logback/logback-classic "1.5.18"]]}}
+                                     [ch.qos.logback/logback-classic "1.5.32"]]}}
   :repl-options {:init-ns clj-mu.core})
